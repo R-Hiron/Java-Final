0a09ad9f2949be6eafe6beae5bf26183dc0106e5
R-Hiron <147127082+R-Hiron@users.noreply.github.com>
2024-08-07 13:09:32 -0230

    Initial Commit

---
+++ Buyer.java
@@ -0,0 +1,5 @@
+public class Buyer extends User {
+    public Buyer(String username, String password, String email) {
+        super(username, password, email, "buyer");
+    }
+}
---
+++ Seller.java
@@ -0,0 +1,5 @@
+public class Seller extends User {
+    public Seller(String username, String password, String email) {
+        super(username, password, email, "seller");
+    }
+}
